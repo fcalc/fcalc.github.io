bf0630c3ffb40c9793bb977e319779dffc0f2d59
Goldstein <goldstein.e@protonmail.com>
2018-03-23 21:34:24 +0300

    Add files via upload

---
+++ fc.js
@@ -0,0 +1,61 @@
+var pow = Math.pow;
+var round = Math.round;
+var sqrt = Math.sqrt;
+var PI = Math.PI;
+var abs = Math.abs;
+var ceil = Math.ceil;
+var floor = Math.floor;
+var sin = Math.sin;
+var cos = Math.cos;
+var min = Math.min;
+var max = Math.max;
+var random = Math.random;
+var E = Math.E;
+
+var __exch = /\((\d+(?:\.\d+)?)\s+([a-zA-Z]{3})\s+in\s+([a-zA-Z]{3})\)/;
+var __rates = {};
+
+function ___calc() {
+	let IN = document.getElementById('in');
+	let OUT = document.getElementById('out');
+	let expr = IN.value;
+	let found = expr.match(__exch);
+	while (found) {
+		let curr1 = found[2].toLowerCase();
+		let curr2 = found[3].toLowerCase();
+		let rate = undefined;
+		if (`${curr1}/${curr2}` in __rates) {
+			rate = __rates[`${curr1}/${curr2}`];
+		} else {
+			let req = new XMLHttpRequest();
+			req.open('GET', `https://api.cryptonator.com/api/ticker/${curr1}-${curr2}`, false);
+			req.send();
+			if (req.status == 200) {
+				rate = JSON.parse(req.responseText).ticker.price;
+			}
+		}
+		__rates[`${curr1}/${curr2}`] = rate;
+		if (rate) {
+			expr = expr.replace(__exch, rate * found[1]);
+		} else {
+			expr = expr.replace(__exch, 'WRONGCURR');
+		}
+		found = expr.match(__exch);
+	}
+	try {
+		let ans = eval(expr);
+		if (ans === undefined || expr == '') {
+			ans = '&nbsp;'
+		}
+		if (typeof ans == 'number' || ans == '&nbsp;') {
+			if (ans != '&nbsp;') {
+				OUT.innerHTML = +ans.toFixed(6);
+			}
+		}
+		if (IN.classList.contains('invalid')) {
+			IN.classList.remove('invalid');
+		}
+	} catch (error) {
+		IN.classList.add('invalid');
+	}
+}
