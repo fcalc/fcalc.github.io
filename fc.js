94db16694a619895830699e3aca020c58d366991
Goldstein <goldstein.e@protonmail.com>
2018-04-25 22:49:32 +0300

    Multi-line mode

--- fc.js
+++ fc.js
@@ -15,14 +15,44 @@ var E = Math.E;
 var __exch = /\(([^\s]+)\s+([a-zA-Z]{3})\s+in\s+([a-zA-Z]{3})\)/;
 var __rates = {};
 
-function ___calc() {
-	let IN = document.getElementById('in');
-	let OUT = document.getElementById('out');
-	let EQ = document.getElementById('eq');
+var maxId = 0;
+
+function ___add_input() {
+	let newId = maxId + 1;
+	let newLine = document.createElement('div');
+	newLine.classList = 'fc fc' + newId;
+	document.body.appendChild(newLine);
+	let newLineIn = document.createElement('input');
+	newLineIn.id = 'in' + newId;
+	newLineIn.classList = 'in';
+	newLineIn.setAttribute('onkeyup', '___calc(' + newId + ', event)');
+	let newLineOut = document.createElement('span');
+	newLineOut.id = 'out' + newId;
+	newLineOut.classList = 'out';
+	newLine.appendChild(newLineIn);
+	newLine.appendChild(newLineOut);
+	newLineIn.focus();
+	maxId++;
+	return true;
+}
+
+function ___calc(id, event) {
+	if (event.keyCode === 13 && id == maxId) {
+		return ___add_input();
+	} else if (event.keyCode === 13 || event.keyCode == 40 && id != maxId) {
+		document.getElementById('in' + (id + 1)).focus();
+	}
+	if (event.keyCode === 38 && id != 1) {
+		document.getElementById('in' + (id - 1)).focus();
+	}
+	let IN = document.getElementById('in' + id);
+	let OUT = document.getElementById('out' + id);
 	let expr = IN.value;
+	if (id > 1) {
+		expr = expr.replace(/ans/g, document.getElementById('out' + (id - 1)).innerHTML);
+	}
 	if (expr === '') {
 		OUT.innerHTML = '&nbsp;';
-		EQ.innerHTML = '&ne;';
 		return;
 	}
 	let found = expr.match(__exch);
@@ -58,11 +88,23 @@ function ___calc() {
 				OUT.innerHTML = +ans.toFixed(6);
 			}
 		}
-		EQ.innerHTML = '=';
-		if (IN.classList.contains('invalid')) {
-			IN.classList.remove('invalid');
+		if (OUT.classList.contains('invalid')) {
+			OUT.classList.remove('invalid');
 		}
 	} catch (error) {
-		IN.classList.add('invalid');
+		OUT.classList.add('invalid');
+	}
+}
+
+function ___calc_all() {
+	let id = 1;
+	let finished = false;
+	while (!finished) {
+		if (document.getElementById('in' + id) !== null) {
+			___calc(id, {});
+		} else {
+			finished = true;
+		}
+		id++;
 	}
 }
